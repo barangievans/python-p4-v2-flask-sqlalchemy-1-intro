9396b0c0f22809ce87c9ef4a3b41d703678a3c65
barangievans <barangievans@gmail.com>
2024-10-08 00:55:16 +0300

    intro

--- server/app.py
+++ server/app.py
@@ -1,25 +1,39 @@
 # server/app.py
 
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_migrate import Migrate
+from models import db, Pet
 
-from models import db
-
-# create a Flask application instance
+# Create a Flask application instance
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
+# Configure the database connection to the local file app.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-# configure flag to disable modification tracking and use less memory
+# Configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create a Migrate object to manage schema modifications
+# Create a Migrate object to manage schema modifications
 migrate = Migrate(app, db)
 
-# initialize the Flask application to use the database
+# Initialize the Flask application to use the database
 db.init_app(app)
 
+# Create a sample route to get all pets
+@app.route('/pets', methods=['GET'])
+def get_pets():
+    pets = Pet.query.all()
+    return jsonify([{'id': pet.id, 'name': pet.name, 'species': pet.species} for pet in pets])
 
+# Create a route to add a new pet
+@app.route('/pets', methods=['POST'])
+def add_pet():
+    data = request.get_json()
+    new_pet = Pet(name=data['name'], species=data['species'])
+    db.session.add(new_pet)
+    db.session.commit()
+    return jsonify({'id': new_pet.id, 'name': new_pet.name, 'species': new_pet.species}), 201
+
+# Main entry point to run the application
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
